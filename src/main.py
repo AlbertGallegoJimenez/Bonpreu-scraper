4d6fee0bc17918abdebbee1b31c5fe7d62397bd5
Albert Gallego Jiménez <91949040+AlbertGallegoJimenez@users.noreply.github.com>
2024-11-01 17:59:31 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,8 +6,10 @@ if __name__ == "__main__":
     base_url = "https://www.compraonline.bonpreuesclat.cat"
     # Create the scraper object
     scraper = BonpreuScraper(base_url)
-    # Get the categories section url
-    scraper.get_categories_url()
-    # Get the categories
-    scraper.get_categories()
+    # Get the categories url dictionary
+    categories = scraper.get_categories_url('Frescos')
+    categories_urls = scraper.extract_nested_values(categories)
+    # Get the products info lists
+    products = scraper.get_product_info(categories_urls[0])
+    
     
